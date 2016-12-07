8a06086c45e96f412e5241384a94b7967ee7982b
LIMBYEONGCHAE <ithing108@gmail.com>
2016-12-07 22:31:01 +0900

    Modify quitHandler

--- Assets/Resources/Scripts/QuitHandler.cs
+++ Assets/Resources/Scripts/QuitHandler.cs
@@ -36,6 +36,8 @@ public class QuitHandler : MonoBehaviour {
         Text prevCoin = GameObject.Find("coinValue").GetComponent<Text>();
 
         renew_coin = (int.Parse(newCoin.text) + int.Parse(prevCoin.text)).ToString();
+        //renew_coin = (int.Parse("20") + int.Parse("5")).ToString();
+
         renew_result = result[0] +"/"+ result[1] +"/"+ renew_coin +"/"+ result[3];
         Debug.Log("new : " + newCoin.text + "   prev : " + prevCoin.text);
         Debug.Log("total result : " + renew_result);
@@ -43,7 +45,7 @@ public class QuitHandler : MonoBehaviour {
 
         string url = "http://52.78.164.46/coinUpdate.php";
         WWWForm retData = new WWWForm();
-
+        Debug.Log("result[1]=" + result[1] + "  renew_coin : " + renew_coin);
         retData.AddField("Nick", renew_result[1]);
         retData.AddField("COIN", renew_coin);
 
